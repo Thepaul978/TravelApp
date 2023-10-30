118e1a8ee116e399c299a57a67b59e6fce488a10
Paul <pauloomen1997@gmail.com>
2023-10-30 10:01:31 +0100

    Update SwaggerUI

--- src/main/java/com/example/travelapp/TravelAppApplication.java
+++ src/main/java/com/example/travelapp/TravelAppApplication.java
@@ -1,11 +1,11 @@
 package com.example.travelapp;
 
+import com.example.travelapp.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableSwagger2
 public class TravelAppApplication {
 
 	public static void main(String[] args) {
